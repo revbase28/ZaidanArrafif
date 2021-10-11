cefe2d82d0fa6e3c245e93c8d6beca2a470f4c51
revbase28 <reihanreinaldimctna@gmail.com>
2021-10-11 15:53:13 +0700

    Restore download surah

--- app/src/main/java/com/revbase/zaidanarrafif/data/DownloadRepoImpl.kt
+++ app/src/main/java/com/revbase/zaidanarrafif/data/DownloadRepoImpl.kt
@@ -2,34 +2,47 @@ package com.revbase.zaidanarrafif.data
 
 import android.content.Context
 import com.revbase.zaidanarrafif.domain.repositories.DownloadRepository
-import java.io.BufferedInputStream
+import java.io.*
 import java.net.URL
 import javax.inject.Inject
+import kotlin.jvm.Throws
 
 class DownloadRepoImpl @Inject constructor(
     private val context: Context
 ): DownloadRepository {
+
+    @Throws(IOException::class)
     override suspend fun downloadAudioFromUrl(
         surahName: String,
         downloadUrl: String,
         fileName: String
     ) {
         val url = URL(downloadUrl)
-        val filename = "${fileName}.mp3"
-        val outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE)
-        val connection = url.openConnection()
-        connection.connect()
-        val inputStream = BufferedInputStream(url.openStream())
-        val data = ByteArray(1024)
-        var count = inputStream.read(data)
-        var total = count
-        while (count != -1) {
-            outputStream.write(data, 0, count)
-            count = inputStream.read(data)
-            total += count
+        val downloadDir = File("${context.filesDir}/$surahName")
+        if(!downloadDir.exists()) {
+            downloadDir.mkdirs()
         }
-        outputStream.flush()
-        outputStream.close()
-        inputStream.close()
+        val downloadFile = File(downloadDir, "$fileName.mp3")
+        val outputStream = FileOutputStream(downloadFile)
+        var inputStream: BufferedInputStream? = null
+        try {
+            val connection = url.openConnection()
+            connection.connect()
+            inputStream = BufferedInputStream(url.openStream())
+            val data = ByteArray(1024)
+            var count = inputStream.read(data)
+            var total = count
+            while (count != -1) {
+                outputStream.write(data, 0, count)
+                count = inputStream.read(data)
+                total += count
+            }
+        } catch (e: IOException) {
+            throw IOException("Download gagal karena tidak ada koneksi")
+        } finally {
+            outputStream.flush()
+            outputStream.close()
+            inputStream?.close()
+        }
     }
 }
